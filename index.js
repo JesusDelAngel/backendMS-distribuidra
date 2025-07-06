90b5e77a37557a57843a8dcdfb06d8a31da0fd7b
Jesus-Del-Ange <jesusdelangel235@gmail.com>
2025-07-05 18:07:43 -0600

    Configuracion para rutas estaticas uploads

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import routes from './routes/index.js'
 import mongoose from'mongoose';
 import bodyParser from 'body-parser'
+import cors from 'cors';
 
 // Conectar mongo
 //Conectar a mongo
@@ -11,14 +12,18 @@ mongoose.connect('mongodb://localhost/DistribuidoraAPI',{
 });
 // crear el servidor
 const app = express();
+app.use(cors()); // Esto habilita CORS para todas las rutas
 
 //Habilitar bodyparser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+
 // Routing // get obtiene una ruta en especifico, USE trata de acceder a  todas las rutas que inicien con /
 app.use('/',routes)// app.use('/',usuarioRoutes)
 
+//Carpeta Publica
+app.use(express.static('uploads'));
 //Definir un puerto y arrancar el proyecto
 const port = 3000;
 
